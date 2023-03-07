32bfc99e67160d64edb1f1e418a864f23a5c9a3e
Etowww <etow.engineering@gmail.com>
2023-03-07 14:45:03 -0700

    Added the post route for creating and rendering new notes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -12,4 +12,23 @@ app.get('/notes', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
+
+app.post('/notes', (req, res) => {
+    const { title, text } = req.body;
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            id: uuidv4(),
+        };
+        readAndAppend(newNote, './db/db.json');
+        res.json(`Note added Successfully!`);
+    } else {
+        res.status(400).json({ error: 'Error in adding note'})
+    };
+});
+
+
+
+
 module.exports = app;
